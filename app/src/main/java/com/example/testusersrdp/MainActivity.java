a6572ec5c137e75701f5a91f9cb0351a668a7ffe
Никита <nikita.slepnev.98@gmail.com>
2021-10-10 15:45:27 +0800

    Добавлена возможность отображения курсов и их прокрутки

--- app/src/main/java/com/example/testusersrdp/MainActivity.java
+++ app/src/main/java/com/example/testusersrdp/MainActivity.java
@@ -7,16 +7,19 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 import com.example.testusersrdp.adapter.CategoryAdapter;
+import com.example.testusersrdp.adapter.CourseAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import model.Category;
+import model.Course;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView categoryRecycler;
+    RecyclerView categoryRecycler, courseRecycler;
     CategoryAdapter categoryAdapter;
+    CourseAdapter  courseAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,8 +30,22 @@ public class MainActivity extends AppCompatActivity {
         categoryList.add(new Category(2, "Сайты"));
         categoryList.add(new Category(3, "Языки"));
         categoryList.add(new Category(4, "Прочее"));
-
         setCategoruRecycler(categoryList);
+
+        List<Course> courseList = new ArrayList<>();
+        courseList.add(new Course(1, "course1", "Профессия Java\nразработчик", "1 января", "начальный", "#424345"));
+        courseList.add(new Course(2, "ccharp_1", "Профессия C#\nразрботчик", "1 января", "начальный", "#611884"));
+        courseList.add(new Course(3, "python", "Профессия C++\nразрботчик", "1 января", "начальный", "#9FA52D"));
+
+        setCourseRecycler(courseList);
+    }
+
+    private void setCourseRecycler(List<Course> courseList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        courseRecycler = findViewById(R.id.courseRecycler);
+        courseRecycler.setLayoutManager(layoutManager);
+        courseAdapter = new CourseAdapter(this, courseList);
+        courseRecycler.setAdapter(courseAdapter);
     }
 
     private void setCategoruRecycler(List<Category> categoryList) {
