a6d921ac1d40967d0d8b518cce00e1f003f423ef
Никита <nikita.slepnev.98@gmail.com>
2021-10-10 13:21:41 +0800

    Добавлено отображение категорий с возможностью прокрутки по горизонтали

--- app/src/main/java/com/example/testusersrdp/MainActivity.java
+++ app/src/main/java/com/example/testusersrdp/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.testusersrdp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.testusersrdp.adapter.CategoryAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Category;
+
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView categoryRecycler;
+    CategoryAdapter categoryAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, "Игры"));
+        categoryList.add(new Category(2, "Сайты"));
+        categoryList.add(new Category(3, "Языки"));
+        categoryList.add(new Category(4, "Прочее"));
+
+        setCategoruRecycler(categoryList);
+    }
+
+    private void setCategoruRecycler(List<Category> categoryList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+        categoryAdapter = new CategoryAdapter(this, categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
     }
 }
